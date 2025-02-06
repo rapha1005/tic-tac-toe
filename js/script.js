94496cee4811ef3cd9e57b48e5133239399362fb
rapha1004 <106275964+rapha1004@users.noreply.github.com>
2025-02-06 17:17:33 +0100

    quelque update

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ const $startGameVsPlayerButtons = document.querySelector('.new-game-screen-new-g
 const $startingScreen = document.querySelector('.new-game-screen')
 const $gameScreen = document.querySelector('.game-screen')
 const $gameCells = document.querySelectorAll('.game-screen-grid__cell')
-const $playerIndicator = document.querySelector('.game-screen-header-turn-indicator svg')
 const $resetButton = document.querySelector('.game-screen-header-restart-btn')
 const $turnIndicator = document.querySelector('.game-screen-header-turn-indicator')
 const $endModal = document.querySelector('.game-screen-end-game-modal')
-
+const $player2Score = document.querySelector('.player2Score')
+const $player1Score = document.querySelector('.player1Score')
 
 const player1 = `<svg width="32" height="32" viewBox="0 0 32 32" fill="#ffc860" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M31.9704 15.8706C31.9704 7.10551 24.8649 0 16.0998 0C7.33476 0 0.229248 7.10551 0.229248 15.8706C0.229248 24.6357 7.33476 31.7412 16.0998 31.7412C24.8649 31.7412 31.9704 24.6357 31.9704 15.8706ZM9.63405 15.8706C9.63405 12.2996 12.5289 9.4048 16.0998 9.4048C19.6708 9.4048 22.5656 12.2996 22.5656 15.8706C22.5656 19.4416 19.6708 22.3364 16.0998 22.3364C12.5289 22.3364 9.63405 19.4416 9.63405 15.8706Z" fill="#F2B137"/>
@@ -25,6 +25,8 @@ let currentPlayer = "x"
 let round = 0
 let player1Score = []
 let player2Score = []
+let player1GScore = 0
+let player2GScore = 0
 
 $resetButton.addEventListener('click', () => gameStart())
 
@@ -59,6 +61,8 @@ $gameCells.forEach(function ($gameCell) {
                     setTimeout(() => {
                         document.querySelector('.game-screen-end-game-modal__title').innerHTML = player1 + "takes the round"
                         document.querySelector('.game-screen-end-game-modal__title').style.color = "#ffc860"
+                        player1GScore += 1
+                        $player1Score.innerHTML = player1GScore
                         $endModal.style.display = "flex"
                     }, 500);
                    
@@ -72,6 +76,9 @@ $gameCells.forEach(function ($gameCell) {
                     setTimeout(() => {
                         document.querySelector('.game-screen-end-game-modal__title').innerHTML = player2 + "takes the round"
                         document.querySelector('.game-screen-end-game-modal__title').style.color = "#40e0d0"
+                        player2GScore += 1
+                        $player2Score.innerHTML = player2GScore
+                        
                         $endModal.style.display = "flex"
                     }, 500);
                     
@@ -100,7 +107,7 @@ function isWin(playerScore) {
 
 function countRound() {
     round++
-    currentPlayer === "o" ? currentPlayer = "x" : currentPlayer = "o"
+    currentPlayer  = currentPlayer === "o" ? "x" : "o"
     console.log('round ' + round)
     console.log('player ' + currentPlayer)
     if (currentPlayer === "o") {
